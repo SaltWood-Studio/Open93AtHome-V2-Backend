ad49a659b24d7ab965804ab5900f8a122674f07d
SALTWOOD <105980161+SALTWOOD@users.noreply.github.com>
2024-08-18 23:33:45 +0800

    fix: Token 验证逻辑错误

--- Open93AtHome/Modules/Request/Token.cs
+++ Open93AtHome/Modules/Request/Token.cs
@@ -14,19 +14,19 @@ namespace Open93AtHome.Modules.Request
 
         [Indexed]
         [Column("bytes")]
-        public byte[] Bytes { get; set; } = new byte[MaxTokenLength];
+        public byte[] Bytes { get; set; } = new byte[MaxTokenLength / 8];
 
         [Column("hasAllPermission")]
         public bool HasAllPermission { get; set; } = false;
 
         public bool CheckPermission(string token, bool allPermission) => 
-            SHA256.HashData(this.Bytes).EqualsAll(Encoding.UTF8.GetBytes(token)) &&
+            this.Bytes.EqualsAll(SHA256.HashData(Encoding.UTF8.GetBytes(token))) &&
                 (this.HasAllPermission || (!this.HasAllPermission && !allPermission));
 
         public static Token Create()
         {
             Token token = new Token();
-            token.Bytes = new byte[MaxTokenLength];
+            token.Bytes = new byte[MaxTokenLength / 8];
             Random random = new Random();
             random.NextBytes(token.Bytes);
             return token;
